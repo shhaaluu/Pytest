b549c19c43c5bd02e33e48a8690765d1ce269486
shhaaluu <117257933+shhaaluu@users.noreply.github.com>
2022-11-02 12:17:49 +0530

    Add files via upload

---
+++ NAK_181_Parameterizing_test/test_parametrize.py
@@ -0,0 +1,14 @@
+import math_func
+import pytest
+
+
+@pytest.mark.parametrize('num1, num2, result',
+                        [
+                            (7, 3, 10),
+                            ('Hello', ' World', 'Hello World'),
+                            (10.5, 25.5, 36)
+                        ]
+                        )
+                        
+def test_add(num1, num2, result):
+    assert math_func.add(num1, num2) == result
