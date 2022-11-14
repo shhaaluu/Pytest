3dfefb4413ce611ca5a5eac9fb827344f1a3885e
shhaaluu <117257933+shhaaluu@users.noreply.github.com>
2022-11-14 11:45:28 +0530

    Delete Python directory

--- Python/File Handling/Q2.py
+++
@@ -1,8 +0,0 @@
-# Write a Python program to read a file line by line and store it into a list.
-
-def file_read1(fname):
-    with open(fname) as f:
-        content_list = f.readlines()
-        print(content_list)
-
-file_read1('test.txt')
